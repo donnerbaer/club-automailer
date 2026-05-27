4665fdf2377c430acfed615e8fff7ebae765ef18
donnerbaer <45535297+donnerbaer@users.noreply.github.com>
2026-05-27 21:43:28 +0200

    Update version.py

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
-""" This module contains the version information for the email notificator tool. """
+""" This module contains the version information for the club automailer. """
 
-__version__ = "v0.1.0"
+__version__ = "v0.1.1"
 
 
 def get_version():
@@ -8,4 +8,4 @@ def get_version():
 
 
 if __name__ == "__main__":
-    print(f"Email Notificator Tool Version: {get_version()}")
+    print(f"Club Automailer Version: {get_version()}")
